67cc2c949e093f6c8dcaf6d6b76dadf609659c5f
analyte11 <2584115896@qq.com>
2026-07-21 23:20:43 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE="lulu-v1";
-const URLs=["index.html","manifest.json","icons/icon-192.png","icons/icon-512.png"];
-self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLs)}).then(self.skipWaiting()))});
-self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){if(k!==CACHE)return caches.delete(k)}))}).then(self.clients.claim()))});
-self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
